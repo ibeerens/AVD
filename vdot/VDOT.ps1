bc4a3458c1f93d75e6b718b60b135b1fb79c66ef
Ivo Beerens <info@ivobeerens.nl>
2022-03-22 11:28:37 +0100

    Config change

--- vdot/VDOT.ps1
+++ vdot/VDOT.ps1
@@ -2,7 +2,8 @@
     .SYNOPSIS
         Virtual Desktop Optimalization Tool (VDOT)
     .DESCRIPTION
-        Download the Virtual Desktop Optimalization Tool (VDOT), creates a folder called optimize and runs VDOT 
+        Download the Virtual Desktop Optimalization Tool (VDOT), creates a folder called optimize and runs VDOT tool.
+        The VDOT tool determines OS version at run-time
     .NOTES
         Version:        1.0
         Author:         Ivo Beerens
@@ -17,6 +18,7 @@
         https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
         https://docs.microsoft.com/en-us/windows-server/remote/remote-desktop-services/rds-vdi-recommendations-2004
     .Example
+        Script needs to be run with PowerShell elevated
 #>
 
 # Variables
@@ -24,6 +26,8 @@ $verbosePreference = 'Continue'
 $vdot = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip' 
 $vdot_location = 'c:\Optimize' 
 $vdot_location_zip = 'c:\Optimize\vdot.zip'
+$apppackages = 'https://raw.githubusercontent.com/ibeerens/AVD/main/vdot/AppxPackages.json'
+$apppackages_location = 'C:\Optimize\AppxPackages.json'
 
 # Enable TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -47,10 +51,18 @@ Expand-Archive $vdot_location_zip -DestinationPath $vdot_location -Verbose -Forc
 # Remove Archive
 Remove-Item $vdot_location_zip
 
+# Download AppPackages.json
+Invoke-WebRequest -Uri $apppackages -OutFile $apppackages_location
+
+# Copy the AppPackage file to all versions
+Copy-Item $apppackages_location -Destination 'C:\Optimize\Virtual-Desktop-Optimization-Tool-main\1909\ConfigurationFiles\AppxPackages.json'
+Copy-Item $apppackages_location -Destination 'C:\Optimize\Virtual-Desktop-Optimization-Tool-main\2004\ConfigurationFiles\AppxPackages.json'
+Copy-Item $apppackages_location -Destination 'C:\Optimize\Virtual-Desktop-Optimization-Tool-main\2009\ConfigurationFiles\AppxPackages.json'
+
 # Unblock all files
 dir $vdot_location -Recurse | Unblock-File
 
-# Change folder to VDOT
+# Change folder to VDOT folder
 $vdot_folder = $vdot_location + '\Virtual-Desktop-Optimization-Tool-main' 
 cd $vdot_folder
 
